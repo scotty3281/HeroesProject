71b213db098a7a17ed1a3736530f963e63db3541
Jordan SCott <Jordan SCott>
2017-04-29 20:32:39 -0400

    create main menu enum

---
+++ HeroesProject/MenuActionItme.cs
@@ -0,0 +1,17 @@
+﻿namespace HeroesProject
+{
+    partial class Program
+    {
+        private enum MenuActionItme : int
+        {
+            invalid = 0,
+            repeat = 1,
+
+            maps = 1,
+            heroes = 2,
+            builds = 3,
+            stats = 4,
+            exit = 5,
+        }
+    }
+}
--- HeroesProject/Program.cs
+++ HeroesProject/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace HeroesProject
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
+            bool quit = false;
+            Console.Title = "HotS Stats";
+            while (!quit)
+            {
+
+            }
         }
+
+        private static readonly string menu =
+            "Choose an option:\n" +
+            "1) Maps\n" +
+            "2) Heroes\n" +
+            "3) Builds\n" +
+            "4) Stats\n" +
+            "5) Exit\n";
     }
 }
